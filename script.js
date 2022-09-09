7c8a54dbc838b23b850d78e4a533031035adfa4c
Federico Krenn <fedekrenn@gmail.com>
2022-09-09 14:21:40 -0300

    Función para sortear el número principal, se acomodan estilos

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
-function fiftennNumbersAleatory() {
+const userboard = document.getElementById('playerBoard')
+const cpuBoard = document.getElementById('cpuBoard')
+const totalNumbers = document.getElementById('totalNumbers')
+const numberDraw = document.getElementById('numberDraw')
+
+const userNumbers = fifteenRandomNumbers()
+const cpuNumbers = fifteenRandomNumbers()
+
+for (let number of userNumbers) {
+    userboard.innerHTML += `<div class="numero-contenedor__child">${number}</div>`
+}
+
+for (let number of cpuNumbers) {
+    cpuBoard.innerHTML += `<div class="numero-contenedor__child">${number}</div>`
+}
+
+numberDraw.addEventListener('click', randomNumberDraw)
+randomNumberDraw()
+
+
+
+function randomNumberDraw() {
+
+    const randomNumber = Math.floor(Math.random() * 100)
+
+    numberDraw.innerText = randomNumber
+
+    totalNumbers.innerHTML += `<div class="numero-obtenido">${randomNumber}</div>`
+}
+
+
+
+
+function fifteenRandomNumbers() {
 
     const arr = [];
 
     for (let i = 0; i < 15; i++) {
-        const number = Math.floor(Math.random() * 100)
+        const randomNumber = Math.floor(Math.random() * 100)
 
-        isInArray = arr.includes(number)
+        isInArray = arr.includes(randomNumber)
 
-        if (number == 100) {
+        if (randomNumber == 100 || randomNumber == 0) {
+            i-- // Si bien no lo agrega al array por el continue, con el i-- "vuelvo" un ciclo, ya que si no lo pierdo y quedo con menos de 15
             continue
         } else if (!isInArray) {
-            arr.push(number)
+            arr.push(randomNumber)
         } else {
             i--
         }
     }
+
     return arr
 }
-
-function printNumbersInBoard() {
-
-    const userboard = fiftennNumbersAleatory()
-    const cpuBoard = fiftennNumbersAleatory()
-
-    const user = document.getElementById('playerBoard')
-    const cpu = document.getElementById('cpuBoard')
-
-    for (let number of userboard) {
-        user.innerHTML += `<div class="numero-contenedor__child">${number}</div>`
-    }
-
-    for (let number of cpuBoard) {
-        cpu.innerHTML += `<div class="numero-contenedor__child">${number}</div>`
-    }
-}
-
-printNumbersInBoard()
