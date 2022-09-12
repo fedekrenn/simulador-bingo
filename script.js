af8cf1e6a71f12213340b5c8d766ce2f9b53a4b7
Federico Krenn <fedekrenn@gmail.com>
2022-09-12 18:52:04 -0300

    Se añade lógica para ir descontando bolas del bolillero e ir marcando las que salieron

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ const numberDraw = document.getElementById('numberDraw')
 
 const userNumbers = fifteenRandomNumbers()
 const cpuNumbers = fifteenRandomNumbers()
+let userAcerts = 0
+let cpuAcerts = 0
 
-for (let number of userNumbers) {
-    userboard.innerHTML += `<div class="numero-contenedor__child">${number}</div>`
+// Creo el array con todos los numeros del 1 al 99, ya que son las bolas que pueden salir
+const balls = []
+for (let i = 1; i < 100; i++) {
+    balls.push(i)
 }
 
-for (let number of cpuNumbers) {
-    cpuBoard.innerHTML += `<div class="numero-contenedor__child">${number}</div>`
+for (let i = 0; i < userNumbers.length; i++) {
+    userboard.innerHTML += `<div class="numero-contenedor__child" id='userNum${i}'>${userNumbers[i]}</div>`
 }
+
+for (let i = 0; i < cpuNumbers.length; i++) {
+    cpuBoard.innerHTML += `<div class="numero-contenedor__child" id='cpuNum${i}'>${cpuNumbers[i]}</div>`
+}
+
 
 numberDraw.addEventListener('click', randomNumberDraw)
 randomNumberDraw()
@@ -20,16 +29,41 @@ randomNumberDraw()
 
 
 function randomNumberDraw() {
-
-    const randomNumber = Math.floor(Math.random() * 100)
+    const randomNumber = balls[Math.floor(Math.random() * balls.length)]
+    balls.splice(balls.indexOf(randomNumber), 1)
+    console.log(balls)
 
     numberDraw.innerText = randomNumber
 
     totalNumbers.innerHTML += `<div class="numero-obtenido">${randomNumber}</div>`
+
+    if (userNumbers.includes(randomNumber)) {
+        document.getElementById(`userNum${userNumbers.indexOf(randomNumber)}`).style.backgroundColor = 'green'
+        userAcerts++
+    }
+
+    if (cpuNumbers.includes(randomNumber)) {
+        document.getElementById(`cpuNum${cpuNumbers.indexOf(randomNumber)}`).style.backgroundColor = 'red'
+        cpuAcerts++
+    }
+
+    checkWinner()
+
+    if (balls.length === 0) {
+        alert('No quedan mas bolas!')
+        document.getElementById('numberDraw').style.display = 'none'
+    }
 }
 
-
-
+function checkWinner() {
+    if (userAcerts === 15) {
+        alert('Ganaste!')
+        document.getElementById('numberDraw').style.display = 'none'
+    } else if (cpuAcerts === 15) {
+        alert('Perdiste, la ganadora es la CPU!')
+        document.getElementById('numberDraw').style.display = 'none'
+    }
+}
 
 function fifteenRandomNumbers() {
 
